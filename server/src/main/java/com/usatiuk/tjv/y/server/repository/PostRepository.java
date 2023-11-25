d0c2e2d6f3b8d770b08a88cfad762e068c9f50f9
Stepan Usatiuk <usatiste@fit.cvut.cz>
2023-11-25 21:50:10 +0100

    use paging repo

--- server/src/main/java/com/usatiuk/tjv/y/server/repository/PostRepository.java
+++ server/src/main/java/com/usatiuk/tjv/y/server/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.usatiuk.tjv.y.server.repository;
 
 import com.usatiuk.tjv.y.server.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Collection;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long>, CrudRepository<Post, Long> {
     Collection<Post> findByAuthorUuid(String authorUuid);
+
+    Page<Post> findByAuthorUuid(String authorUuid, Pageable pageable);
 }
